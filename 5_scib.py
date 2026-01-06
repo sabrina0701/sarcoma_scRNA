63d9fd747876e1e09de8982abb788ffcdfcaa59e
sabrina0701 <88844335+sabrina0701@users.noreply.github.com>
2026-01-06 15:21:09 +0100

    Update 5_scib.py

--- 5_scib.py
+++ 5_scib.py
@@ -1,6 +1,6 @@
 ########### Python script for scib. ###########
-########### Created by Danwen Qian on 4-09-2023. ###########
-########### Last modified by Danwen Qian on 4-09-2023. ###########
+########### Created by Danwen Qian on 4-09-2025. ###########
+########### Last modified by Danwen Qian on 4-09-2025. ###########
 
 import numpy as np
 import pandas as pd
@@ -41,31 +41,3 @@ bm.plot_results_table(min_max_scale=False,save_dir="/mnt/beegfs01/scratch/d_qian
 df = bm.get_results(min_max_scale=False)
 df.to_csv("/mnt/beegfs01/scratch/d_qian/SingleCell/combined_datasets/metrics_immune.csv")
 
-
-
-
-adata = sc.read_h5ad("/mnt/beegfs01/scratch/d_qian/SingleCell/combined_datasets/immune_integrated_SCT.h5ad")
-
-adata.obsm["Unintegrated"] = adata.obsm["X_pca"]
-adata.obsm["CCA"] = adata.obsm["X_integrated.cca"]
-adata.obsm["rPCA"] = adata.obsm["X_integrated.rpca"]
-adata.obsm["Harmony"] = adata.obsm["X_harmony"]
-
- 
-bm = Benchmarker(
-    adata,
-    batch_key="study",
-    label_key="scATOMIC_pred",
-    embedding_obsm_keys=["Unintegrated", "CCA","rPCA","Harmony"],
-    pre_integrated_embedding_obsm_key="X_pca",
-    bio_conservation_metrics=BioConservation(),
-    batch_correction_metrics=BatchCorrection(),
-    n_jobs=1,
-)
-bm.benchmark()
-
-bm.plot_results_table(min_max_scale=False,save_dir="/mnt/beegfs01/scratch/d_qian/SingleCell/plots")
-
-
-df = bm.get_results(min_max_scale=False)
-df.to_csv("/mnt/beegfs01/scratch/d_qian/SingleCell/combined_datasets/metrics_Myeloid_SCT.csv")
